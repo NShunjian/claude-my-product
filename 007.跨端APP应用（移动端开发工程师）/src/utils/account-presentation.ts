fedfa1ccc2ef7645b5cc35b848e964d1c72b2993
apple <apple@local>
2026-08-31 17:17:07 +0800

    feat(uniapp): utils 翻译 React版 (date/format/validators/id)

---
+++ 007.跨端APP应用（移动端开发工程师）/src/utils/account-presentation.ts
@@ -0,0 +1,1 @@
+export function accountAccent(_type: string): string { return '#2E7DE6' }
---
+++ 007.跨端APP应用（移动端开发工程师）/src/utils/category-presentation.ts
@@ -0,0 +1,3 @@
+export function categoryIconColor(icon: string, color: string) {
+  return { icon, color }
+}
